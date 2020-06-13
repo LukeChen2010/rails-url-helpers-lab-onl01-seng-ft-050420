375edb3aad12e2a5f6030c531c51f57e73d6c725
Luke Chen <luke.chen.2010@gmail.com>
2020-06-13 18:46:17 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,7 @@ class StudentsController < ApplicationController
   def activate
     @student = Student.find_by(id: params[:id])
     @student.active = !@student.active
+    @student.save
     redirect_to student_path(@student)
   end
 
